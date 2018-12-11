848bcd06047bc20114dc0374b3f96f1291edfaa3
bnewbs28 <bnewbs28@gmail.com>
2018-12-10 18:05:17 -0600

    modified stepper_move.h

--- ece453_software/apps/drivers/buttons.c
+++ ece453_software/apps/drivers/buttons.c
@@ -15,7 +15,7 @@ int buttons_read_all (void)
 int buttons_read_red (void)
 {
   return (ece453_reg_read(BUTTONS_REG) & BUTTONS_RED) >> 3;
-} 
+}
 
 int buttons_read_orange (void)
 {
@@ -34,13 +34,13 @@ int buttons_read_blue (void)
 
 
 //*****************************************************************************
-// SWITCHES FOR XY PLOTTER 
+// SWITCHES FOR XY PLOTTER
 //*****************************************************************************
 int buttons_read_x_min (void)
 {
 //  printf("DEBUG: buttons_read_x_min [%i] masked [%i] shifted [%i]\n", ece453_reg_read(SWITCHES_REG), (ece453_reg_read(SWITCHES_REG) & BUTTONS_X_MIN),  (ece453_reg_read(SWITCHES_REG) & BUTTONS_X_MIN));
   return (ece453_reg_read(SWITCHES_REG) & BUTTONS_X_MIN);
-} 
+}
 
 int buttons_read_x_max (void)
 {
@@ -57,5 +57,3 @@ int buttons_read_y_max (void)
 {
   return (ece453_reg_read(SWITCHES_REG) & BUTTONS_Y_MAX) >> 3;
 }
-
-
--- ece453_software/apps/include/stepper_move.h
+++ ece453_software/apps/include/stepper_move.h
@@ -7,6 +7,9 @@
 //*****************************************************************************
 //*****************************************************************************
 int stepper_reset(void);
-int stepper_position(uint32_t x, uint32_t y, uint32_t x_pos, uint32_t y_pos);
+int stepper_position(uint32_t x, uint32_t y);
+int move(uint32_t x, uint32_t y, uint32_t mag);
+int movePath(uint32_t player, uint32_t run, uint32_t moveNum);
+int moveSpaces(uint32_t player, uint32_t startSpace, uint32_t endSpace);
 
 #endif
